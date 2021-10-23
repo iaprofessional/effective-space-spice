e5e7fdbf5dce262cf498e9af253d06d51202398c
iaprofessional <75013595+iaprofessional@users.noreply.github.com>
2021-10-23 11:23:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -25,6 +25,25 @@ function draw() {
     fill("#FF0000");
     stroke("#FF0000");
 
+    circle(rightWristX,rightWristY,20);
+
+    if(rightWristX >0 && rightWristY <= 100) {
+      document.getElementById("speed").innerHTML = "Speed = 0.5x";
+      song.rate(0.5);
+    } else if (rightWristY > 100 && rightWristY <= 200) {
+      document.getElementById("speed").innerHTML = "Speed = 1x";
+      song.rate(1);
+    }else if (rightWristY > 200 && rightWristY <= 300) {
+        document.getElementById("speed").innerHTML = "Speed = 1.5x";
+        song.rate(1.5);
+      }else if (rightWristY > 300 && rightWristY <= 400) {
+        document.getElementById("speed").innerHTML = "Speed = 2x";
+        song.rate(2);
+      }else if (rightWristY > 400 && rightWristY <= 500) {
+        document.getElementById("speed").innerHTML = "Speed = 2.5x";
+        song.rate(2);
+      }
+
     circle(leftWristX,leftWristY,20);
     InNumberleftWrsitY = Number(leftWristY);
     remove_decimals = floor(InNumberleftWrsitY);
@@ -40,7 +59,9 @@ function play() {
 function gotPoses() {
   if(results.length > 0) {
     console.log(results);
-    scoreleftWrist = results[0].pose.keypoints[9].score;
+    scoreRightWrist = results[0].pose.keypoints[10].score;
+    scoreLeftWrist = results[0].pose.keypoints[9].score;
+    console.log("scoreRightWrist = " + scoreRightWrist + "scoreleftwrist = " + scoreLeftWrist);
     console.log("scoreleftWrist = " + scoreleftWrist);
     leftWristX = results[0].pose.lefWrist.x;
     leftWristY = results[0].pose.lefWrist.y;
